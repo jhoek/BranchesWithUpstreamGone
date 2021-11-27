62e8cd42d356c796207b091a8e8615c475991494
Jan Hoek <github@uncommonsense.nl>
2021-11-27 21:36:23 +0100

    Preparations for restructuring

--- Public/Get-BranchesWithUpstreamGone.ps1
+++ Public/Get-BranchesWithUpstreamGone.ps1
@@ -1,7 +1,9 @@
 function Get-BranchesWithUpstreamGone {
-    git fetch --all --prune | Out-Null
-    $Branches = git branch -v | Where-Object { $_.Contains('[gone]') }
-    $BranchNames += $Branches | ForEach-Object {
+    git fetch --all --prune 2>&1 | Out-Null
+
+    git branch -v
+    | Where-Object { $_.Contains('[gone]') }
+    | ForEach-Object {
       if ($_.Split(' ').Item(1) -ne '') {
         $_.Split(' ').Item(1)
       }
@@ -9,6 +11,4 @@ function Get-BranchesWithUpstreamGone {
         $_.Split(' ').Item(2)
       }
     }
-
-    $BranchNames
   }
