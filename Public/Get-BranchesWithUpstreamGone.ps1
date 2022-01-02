a3bb0d3078fc8a57bf9f6a53e1ddb00d20b41d63
Jan Hoek <jhoek@uncommonsense.nl>
2022-01-02 13:34:44 +0100

    Squashed commit of the following:
    commit 34a9a238a8d1f66110ef20fa3a7ca8c0f0ae039c
    Author: Jan Hoek <jhoek@uncommonsense.nl>
    Date:   Sun Jan 2 13:33:19 2022 +0100
    
        Added parameters, support ShouldProcess
        - Allow repo directory to be specified
        - Allow fetch to be skipped
        - ShouldProcess enables -WhatIf and -Confirm
        - Set ConfirmImpact to Medium
    
    commit a363cc9421ae1c00700dc015dd149fff7418aeb2
    Author: Jan Hoek <jhoek@uncommonsense.nl>
    Date:   Sun Jan 2 13:31:38 2022 +0100
    
        Added parameters, capture output
        - Allow repo directory to be specified
        - Allow fetch to be skipped
        - Capture fetch output, only show in case of errors
    
    commit 809c48fe5e1059980ff31f8f8d69c32ba0ae2655
    Author: Jan Hoek <jhoek@uncommonsense.nl>
    Date:   Sun Jan 2 13:30:29 2022 +0100
    
        Added launch configuration

--- Public/Get-BranchesWithUpstreamGone.ps1
+++ Public/Get-BranchesWithUpstreamGone.ps1
@@ -1,14 +1,30 @@
-function Get-BranchesWithUpstreamGone {
-    git fetch --all --prune 2>&1 | Out-Null
+function Get-BranchesWithUpstreamGone
+{
+    param
+    (
+        [Parameter(Position = 0)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Directory = '.',
 
-    git branch -v
+        [switch]$SkipFetch
+    )
+
+    if (-not $SkipFetch)
+    {
+        $FetchOutput = git -C $Directory fetch --all --prune 2>&1
+        if ($LastExitCode) { throw $FetchOutput }
+    }
+
+    git -C $Directory branch -v
     | Where-Object { $_.Contains('[gone]') }
     | ForEach-Object {
-      if ($_.Split(' ').Item(1) -ne '') {
-        $_.Split(' ').Item(1)
-      }
-      else {
-        $_.Split(' ').Item(2)
-      }
+        if ($_.Split(' ').Item(1) -ne '')
+        {
+            $_.Split(' ').Item(1)
+        }
+        else
+        {
+            $_.Split(' ').Item(2)
+        }
     }
-  }
+}
--- Public/Remove-BranchesWithUpstreamGone.ps1
+++ Public/Remove-BranchesWithUpstreamGone.ps1
@@ -1,8 +1,22 @@
-# FIXME: Add ShouldProcess
+function Remove-BranchesWithUpstreamGone
+{
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Medium')]
+    param
+    (
+        [Parameter(Position = 0)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Directory = '.',
 
-function Remove-BranchesWithUpstreamGone {
-    (Get-BranchesWithUpstreamGone) | ForEach-Object {
-      Write-Host "Removing branch $_"
-      git branch $_ -D
+        [switch]$SkipFetch
+    )
+
+    Get-BranchesWithUpstreamGone `
+        -Directory $Directory `
+        -SkipFetch:$SkipFetch
+    | ForEach-Object {
+        if ($PSCmdlet.ShouldProcess("Branch $_ would be deleted.", "Delete branch $_", 'Remove-BranchesWithUpstreamGone'))
+        {
+            git -C $Directory branch $_ -D
+        }
     }
-  }
+}
